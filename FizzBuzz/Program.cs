c28ea6870eb30650d9133083c4d8908e26c6407c
Kiang Kuang <kiangkuang@gmail.com>
2016-07-14 16:43:47 +0800

    refactored code

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(GetResult(i));
+            }
+        }
+
+        public static string GetResult(int num)
+        {
+            if (num % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (num % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (num % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return num.ToString();
+            }
+        }
+    }
+}
---
+++ FizzBuzzTest/Test.cs
@@ -0,0 +1,20 @@
+﻿using FizzBuzz;
+using NUnit.Framework;
+
+namespace FizzBuzzTest
+{
+    [TestFixture]
+    public class Test
+    {
+        [TestCase(3, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(1, "1")]
+        [TestCase(-3, "Fizz")]
+        [TestCase(0, "FizzBuzz")]
+        public void TestFizzBuzz(int input, string expected)
+        {
+            Assert.AreEqual(expected, Program.GetResult(input));
+        }
+    }
+}
